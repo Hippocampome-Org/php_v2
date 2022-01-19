242cc56116561f1ccef1d9d4daabc9c62f28073a
Diek Wheeler <drdiek@mac.com>
2022-01-19 14:58:22 -0500

    Added explanatory page of differences between v1.0 and v2.0

---
+++ Help_v1p0_vs_v2p0.php
@@ -0,0 +1,122 @@
+<?php
+  include ("permission_check.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Hippocampome.org v1.0 vs v2.0</title>
+<script type="text/javascript" src="style/resolution.js"></script>
+<style>
+<!--
+ /* Font Definitions */
+ @font-face
+    {font-family:Wingdings;
+    panose-1:5 0 0 0 0 0 0 0 0 0;}
+@font-face
+    {font-family:Wingdings;
+    panose-1:5 0 0 0 0 0 0 0 0 0;}
+@font-face
+    {font-family:Calibri;
+    panose-1:2 15 5 2 2 2 4 3 2 4;}
+ /* Style Definitions */
+ p.MsoNormal, li.MsoNormal, div.MsoNormal
+    {margin-top:0in;
+    margin-right:0in;
+    margin-bottom:10.0pt;
+    margin-left:0.15in;
+    line-height:115%;
+    font-size:9.5pt;
+    font-family:"Calibri","sans-serif";}
+p.MsoListParagraph, li.MsoListParagraph, div.MsoListParagraph
+    {margin-top:0in;
+    margin-right:0in;
+    margin-bottom:10.0pt;
+    margin-left:.5in;
+    line-height:115%;
+    font-size:9.5pt;
+    font-family:"Calibri","sans-serif";}
+p.MsoListParagraphCxSpFirst, li.MsoListParagraphCxSpFirst, div.MsoListParagraphCxSpFirst
+    {margin-top:0in;
+    margin-right:0in;
+    margin-bottom:0in;
+    margin-left:.5in;
+    margin-bottom:.0001pt;
+    line-height:115%;
+    font-size:9.5pt;
+    font-family:"Calibri","sans-serif";}
+p.MsoListParagraphCxSpMiddle, li.MsoListParagraphCxSpMiddle, div.MsoListParagraphCxSpMiddle
+    {margin-top:0in;
+    margin-right:0in;
+    margin-bottom:0in;
+    margin-left:.5in;
+    margin-bottom:.0001pt;
+    line-height:115%;
+    font-size:9.5pt;
+    font-family:"Calibri","sans-serif";}
+p.MsoListParagraphCxSpLast, li.MsoListParagraphCxSpLast, div.MsoListParagraphCxSpLast
+    {margin-top:0in;
+    margin-right:0in;
+    margin-bottom:10.0pt;
+    margin-left:.5in;
+    line-height:115%;
+    font-size:9.5pt;
+    font-family:"Calibri","sans-serif";}
+.MsoChpDefault
+    {font-family:"Calibri","sans-serif";}
+.MsoPapDefault
+    {margin-bottom:10.0pt;
+    line-height:115%;}
+@page WordSection1
+    {size:8.5in 11.0in;
+    margin:.5in .5in .5in .5in;}
+div.WordSection1
+    {page:WordSection1;}
+ /* List Definitions */
+ ol
+    {margin-bottom:0in;}
+ul
+    {margin-bottom:0in;}
+-->
+</style>
+</head>
+
+<body lang=EN-US link=blue vlink=purple style='tab-interval:.5in'>
+
+<!-- COPY IN ALL PAGES -->
+<?php 
+    include ("function/title.php");
+    include ("function/menu_main.php");
+?>
+
+        
+<BR><BR><BR><BR><BR>
+        
+<div class=WordSection1>
+
+<p class=MsoNormal><b style='mso-bidi-font-weight:normal'><span
+style='font-size:24.0pt;line-height:115%;font-family:"Arial","sans-serif"'>Hippocampome.org v1.0 vs v2.0<o:p></o:p></span></b></p>
+
+<table width=1200>
+    <tr>
+        <td width='90%'>
+            <p class=MsoNormal><span style='font-size:14.0pt;line-height:115%;font-family:
+            "Arial","sans-serif"'>Hippocampome.org v1.0 was designed to be a knowledge base of neuron types in the rodent hippocampus, where the user is able to browse or search for pieces of knowledge that are based on pieces of evidence, i.e. data.<o:p></o:p></span></p>
+        </td>
+    </tr>
+</table>
+
+<table width=1200>
+    <tr>
+        <td width='90%'>
+            <p class=MsoNormal><span style='font-size:14.0pt;line-height:115%;font-family:
+            "Arial","sans-serif"'>Hippocampome.org v2.0 is designed to be a model-ready simulation system, where the user is able to select desired modeling parameters and then download a set of simulation-ready scripts. All of v2.0 is supported by the framework established in v1.0.<o:p></o:p></span></p>
+        </td>
+    </tr>
+</table>
+
+</div>
+
+</body>
+
+</html>
