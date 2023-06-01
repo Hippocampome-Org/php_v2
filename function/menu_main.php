00ecdefce1aa26263331ebb13da72439a4f60a65
Nate Sutton <natemsutton@gmail.com>
2023-06-01 14:57:09 -0400

    fixed menu entry

--- function/menu_main.php
+++ function/menu_main.php
@@ -36,13 +36,14 @@ session_start();
 			<li><a href="<?php echo $menuaddr ?>markers.php">Molecular markers</a></li>
             <li><a href="<?php echo $menuaddr ?>ephys.php">Membrane biophysics</a></li>
             <li><a href="<?php echo $menuaddr ?>connectivity.php">Connectivity</a></li>
-            <li><a href="<?php echo $menuaddr ?>synaptome.php">Synaptic physiology</a></li>
+            <li><a href="<?php echo $menuaddr ?>synaptome_modeling.php">Synaptic physiology</a></li>
 	        <li><a href="<?php echo $menuaddr ?>firing_patterns.php">Firing patterns</a></li>
 	        <li><a href="<?php echo $menuaddr ?>Izhikevich_model.php">Izhikevich models</a></li>
             <li><a href="<?php echo $menuaddr ?>synapse_probabilities.php">Synapse probabilities</a></li>
             <li><a href="<?php echo $menuaddr ?>phases.php">In vivo recordings</a></li>
 	        <li><a href="<?php echo $menuaddr ?>cognome/index.php">Cognome</a></li>
 	        <li><a href="<?php echo $menuaddr ?>counts.php">Neuron type census</a></li>
+	        <li><a href="<?php echo $menuaddr ?>simulation_parameters.php">Simulation parameters</a></li>
 		</ul></li>
 	
 		<li class="topmenu"><a href="<?php echo $menuaddr ?>search.php?searching=1" style="height:32px;line-height:32px;"><span><img src="<?php echo $menuaddr ?>function/menu_support_files/find.png" alt="" id="image_find"/>Search</span></a>
