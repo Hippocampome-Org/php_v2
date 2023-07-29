8b74807aca6605cadc7198a0c1ed5e75253dc94a
Diek Wheeler <drdiek@mac.com>
2023-07-29 02:01:08 -0400

    Added neuron type names to output of API

--- search_engine_json.php
+++ search_engine_json.php
@@ -9,14 +9,21 @@ require_once('SearchEngine/Page.php');
 require_once("access_db.php");
 
 if($_REQUEST['query_str']){
-	$qeueryString =$_REQUEST['query_str'];
+	$queryString =$_REQUEST['query_str'];
 	$test=new Parser();
-	$test->setSearchQuery($qeueryString);
+	$test->setSearchQuery($queryString);
 	$matchingConn=$test->parseQuery();
 	$data = array();
 	for($i=0;$i<count($matchingConn);$i++){
 		$index=$i+1;
-		$data["$index"]=array("source_id"=>$matchingConn[$i]->getSourceId(),"destination_id"=>$matchingConn[$i]->getDestinationId());		
+		$destinationId = $matchingConn[$i]->getDestinationId();
+		if ($destinationId == NULL) {
+			$data["$index"]=array("source_id"=>$matchingConn[$i]->getSourceId(), "source_name"=>$matchingConn[$i]->getSourceName());
+		}
+		else {
+			$data["$index"]=array("source_id"=>$matchingConn[$i]->getSourceId(), "source_name"=>$matchingConn[$i]->getSourceName(), "destination_id"=>$destinationId, "destination_name"=>$matchingConn[$i]->getDestinationName());
+		}
+		// $data["$index"]=array("source_id"=>$matchingConn[$i]->getSourceId(),"destination_id"=>$matchingConn[$i]->getDestinationId());
 	}
 	header('Content-type:application/json;charset=utf-8');
 	echo json_encode($data);
