09dfb727504c6b2e0f8ce398c23d34f27cc7b8ff
Kasturi Nadella <knadella@gmu.edu>
2024-11-19 09:52:26 -0500

    Extended to views per month and year

--- GA_analytics/page_views.php
+++ GA_analytics/page_views.php
@@ -505,7 +505,11 @@ function format_table($conn, $query, $table_string, $csv_tablename, $csv_headers
     }
 
     if (isset($write_file)) {
-        $totalRow = ["Total Count", $count];
+	if($csv_tablename == 'functionality_property_domain_page_views'){
+        	$totalRow = ["Total Count", $matrix_views_count, $evidences_count, $count];
+	}else{
+        	$totalRow = ["Total Count", $count];
+	}
         $csv_rows[] = $totalRow;
 	if($csv_tablename == 'monthly_page_views'){
 		$csv_rows[] = ["pre-Aug 22, 2019",86523];
@@ -3358,11 +3362,10 @@ GROUP BY
     END
 ORDER BY total_views DESC";
 
-	if (($views_request == "views_per_month") || ($views_request == "views_per_year")) {
-    $page_functionality_views_query = "SET SESSION group_concat_max_len = 1000000;
-    SET @sql = NULL;";
-
-    if ($views_request == "views_per_month") {
+  // Check if the request is for monthly or yearly views
+    if (($views_request == "views_per_month") || ($views_request == "views_per_year")) {
+        $page_functionality_views_query = "SET SESSION group_concat_max_len = 1000000; SET @sql = NULL;";
+ if ($views_request == "views_per_month") {
         $page_functionality_views_query .= "SELECT
                 GROUP_CONCAT(DISTINCT
                                 CONCAT(
@@ -3397,96 +3400,69 @@ ORDER BY total_views DESC";
                 ) years;";
     }
 
-    $page_functionality_views_query .= "
-        SET @sql = CONCAT(
-            'SELECT
-                CASE 
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) IN (''morphology'') THEN ''Morphology''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) IN (''morphology_linking_pmid_isbn'') THEN ''Morphology: PMID / ISBN''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) IN (''/synaptome/php/synaptome'') THEN ''Synaptome''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) IN (''connectivity'', ''connectivity_orig'', ''connectivity_test'') THEN ''Connectivity: Known / Potential''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) = ''counts'' THEN ''Census''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) = ''ephys'' THEN ''Membrane Biophysics''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) = ''fp'' THEN ''FP''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) = ''phases'' THEN ''In Vivo''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) = ''markers'' THEN ''Markers''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) = ''synpro'' THEN ''Morphology: Axon and Dendrite Lengths / Somatic Distances''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) IN (''synpro_nm'', ''synpro_nm_old2'') THEN ''Connectivity: Number of Potential Synapses / Number of Contacts / Synaptic Probability''
-                    WHEN SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1) = ''synpro_pvals'' THEN ''Connectivity: Parcel-Specific Tables''
-                    ELSE SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''/property_page_'', -1), ''.'', 1)
-                END AS Property, ', 
-                @sql, ',
-                SUM(REPLACE(page_views, \\'\\', \\'\\')) AS Total_Views
-                FROM temp_combined_analytics 
-                WHERE page LIKE ''%/property_page_%''
-                AND (
-                    LENGTH(
-                        CASE 
-                        WHEN LOCATE(''%'', SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron='', -1), ''&'', 1)) > 0 THEN
-                        SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron='', -1), ''%'', 1)
-                        ELSE
-                        SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron='', -1), ''&'', 1)
-                        END
-                    ) = 4
-                    OR LENGTH(
-                        CASE
-                        WHEN LOCATE(''%'', SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id1_neuron='', -1), ''&'', 1)) > 0 THEN
-                        SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id1_neuron='', -1), ''%'', 1)
-                        ELSE
-                        SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id1_neuron='', -1), ''&'', 1)
-                        END
-                    ) = 4 
-                    OR LENGTH(
-                        CASE
-                        WHEN LOCATE(''%'', SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron_source='', -1), ''&'', 1)) > 0 THEN
-                        SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron_source='', -1), ''%'', 1)
-                        ELSE
-                        SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron_source='', -1), ''&'', 1)
-                        END
-                    ) = 4
-                )
-                AND (
-                    CASE
-                    WHEN LOCATE(''%'', SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron='', -1), ''&'', 1)) > 0 THEN
-                    SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron='', -1), ''%'', 1)
-                    ELSE
-                    SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron='', -1), ''&'', 1)
-                    END NOT IN (4168, 4181, 2232)
-                    OR CASE
-                    WHEN LOCATE(''%'', SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id1_neuron='', -1), ''&'', 1)) > 0 THEN
-                    SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id1_neuron='', -1), ''%'', 1)
-                    ELSE
-                    SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id1_neuron='', -1), ''&'', 1)
-                    END NOT IN (4168, 4181, 2232)
-                    OR CASE
-                    WHEN LOCATE(''%'', SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron_source='', -1), ''&'', 1)) > 0 THEN
-                    SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron_source='', -1), ''%'', 1)
-                    ELSE
-                    SUBSTRING_INDEX(SUBSTRING_INDEX(page, ''id_neuron_source='', -1), ''&'', 1)
-                    END NOT IN (4168, 4181, 2232)
-                )
-                GROUP BY Property
-                ORDER BY Total_Views DESC'
-        );";
+// Combine the dynamically generated SQL into the main query
+$page_functionality_views_query .= "
+    SET @sql = CONCAT(
+        'SELECT 
+            CASE 
+                WHEN page LIKE \"%morphology.php%\" THEN \"Morphology\"
+                WHEN page LIKE \"%markers.php%\" THEN \"Markers\"
+                WHEN page LIKE \"%ephys.php%\" THEN \"Membrane Biophysics\"
+                WHEN page LIKE \"%connectivity.php%\" THEN \"Connectivity: Known / Potential\"
+                WHEN page LIKE \"%synaptome_modeling.php%\" THEN \"Synaptome Modeling\"
+                WHEN page LIKE \"%firing_patterns.php%\" THEN \"Firing Patterns\"
+                WHEN page LIKE \"%izhikevich_model.php%\" THEN \"Izhikevich Model\"
+                WHEN page LIKE \"%synapse_probabilities.php%\" THEN \"Synapse Probabilities\"
+                WHEN page LIKE \"%phases.php%\" THEN \"In Vivo\"
+                WHEN page LIKE \"%/cognome/%\" THEN \"Cognome\"
+                WHEN page LIKE \"%counts.php%\" THEN \"Census\"
+                WHEN page LIKE \"%simulation_parameters.php%\" THEN \"Simulation Parameters\"
+                ELSE \"Other\"
+            END AS Property, ',
+            @sql, ',
+            SUM(REPLACE(page_views, \"\", \"\")) AS Total_Views
+        FROM temp_combined_analytics
+        WHERE 
+            page LIKE \"%/property_page_%\" 
+            OR page LIKE \"%morphology.php%\"
+            OR page LIKE \"%markers.php%\"
+            OR page LIKE \"%ephys.php%\"
+            OR page LIKE \"%connectivity.php%\"
+            OR page LIKE \"%synaptome_modeling.php%\"
+            OR page LIKE \"%firing_patterns.php%\"
+            OR page LIKE \"%izhikevich_model.php%\"
+            OR page LIKE \"%synapse_probabilities.php%\"
+            OR page LIKE \"%phases.php%\"
+            OR page LIKE \"%/cognome/%\"
+            OR page LIKE \"%counts.php%\"
+            OR page LIKE \"%simulation_parameters.php%\"
+        GROUP BY Property
+        ORDER BY Total_Views DESC'
+    );
 
-    $page_functionality_views_query .= "
-        PREPARE stmt FROM @sql;
-        EXECUTE stmt;
-        DEALLOCATE PREPARE stmt;";
-}
+    PREPARE stmt FROM @sql;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+";
+    }
+
 	//echo $page_functionality_views_query;
-	$columns = ['Property', 'Main Matrix Accesses', 'Evidence Accesses', 'Views'];
+	$columns = ['Property', 'Views'];
         $table_string='';
 	$file_name='functionality_property_domain_page_';
         if(isset($write_file)) {
                 if($views_request == 'views_per_month' || $views_request == 'views_per_year'){
 		$columns = ['Property', 'Views'];
                         $file_name .= $views_request;
-                }else{ $file_name .= 'views'; }
-                return format_table($conn, $page_functionality_views_query, $table_string, $file_name, $columns, $neuron_ids=NULL, $write_file, $views_request);
+                	return format_table($conn, $page_functionality_views_query, $table_string, $file_name, $columns, $neuron_ids=NULL, $write_file, $views_request);
+                }else{ $file_name .= 'views';
+			$columns = ['Property', 'Main Matrix Accesses', 'Evidence Accesses', 'Views'];
+                	return format_table($conn, $page_functionality_views_query, $table_string, $file_name, $columns, $neuron_ids=NULL, $write_file, $views_request);
+ 		}
 		//return format_table($conn, $page_functionality_views_query, $table_string, 'functionality_property_domain_page_views', $columns, $neuron_ids=NULL, $write_file);
 	}else{
 		$file_name .= 'views';
+		$columns = ['Property', 'Main Matrix Accesses', 'Evidence Accesses', 'Views'];
 		$table_string = format_table($conn, $page_functionality_views_query, $table_string, $file_name, $columns);
 		$table_string .= get_table_skeleton_end();
 		echo $table_string;
